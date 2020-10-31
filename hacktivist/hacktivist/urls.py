c8d6e34245d8b40470cb53a41f5cf76b0103ce07
Kamalavelan <sskamalavelan@gmail.com>
2020-10-31 17:04:17 +0530

    Add relevant apps
    Signed-off-by: Kamalavelan <sskamalavelan@gmail.com>

--- hacktivist/hacktivist/urls.py
+++ hacktivist/hacktivist/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
+import organizations as org
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('org/', include('organizations.urls')),
 ]
