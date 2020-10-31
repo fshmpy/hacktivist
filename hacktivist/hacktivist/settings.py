fca87fbfa9e6c1de40471451e9177592410075f2
ragulkanth <ragul274@gmail.com>
2020-10-31 19:43:05 +0530

    merge conflict resolved
    Signed-off-by: ragulkanth <ragul274@gmail.com>

--- hacktivist/hacktivist/settings.py
+++ hacktivist/hacktivist/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members',
     'organizations',
+    'events',
+    'members',
 ]
 
 MIDDLEWARE = [
